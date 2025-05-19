ab2208f458453726df769da2b4d91521b7c8e618
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2025-05-19 11:56:01 -0600

    Integracion de mapa, sin marcadores

--- src/components/Modules/Maps.tsx
+++ src/components/Modules/Maps.tsx
@@ -1,9 +1,29 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+};
+
+const center = {
+  lat: 20.659698,
+  lng: -103.349609
+};
+
 function Maps() {
-    return (
-        <div className="bg-blue-300 w-full h-full rounded-lg md:rounded-2xl">
-            Aqui va el mapa
-        </div>
-    )
-}
+  return (
+    <div className="rounded-xl overflow-hidden h-full">
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                <Marker position={center}/>
+            </GoogleMap>
+        </LoadScript>
+    </div>
+  );
+};
 
 export default Maps;
