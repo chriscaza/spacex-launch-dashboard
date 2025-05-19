59de62da9b8945b0ff5a59234f1d0d917120cf4f
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2025-05-19 02:19:59 -0600

    modifique el tamaño de la la letra del boton de limpiar2

--- src/components/Ui/CleanButton.tsx
+++ src/components/Ui/CleanButton.tsx
@@ -6,7 +6,7 @@ function ClearButton({ onClear }: Props) {
   return (
     <button
       onClick={onClear}
-      className="absolute right-6 text-xl font-[var(--font-roboto)] font-bold text-white rounded cursor-pointer"
+      className="absolute right-6 font-[var(--font-roboto)] text-base font-bold text-white rounded cursor-pointer"
     >
       Limpiar
     </button>
