3e390154cc791b32fef08cf7d79f657252b881a5
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2025-05-19 02:13:48 -0600

    Cambie tamaño de letra y padding del LaunchCard

--- src/components/Ui/LaunchCard.tsx
+++ src/components/Ui/LaunchCard.tsx
@@ -13,22 +13,22 @@ type Props = {
 
 function LaunchCard({ launch, rocketName, launchSite }: Props) {
   return (
-    <div className="w-full flex gap-5 bg-white/20 text-white pl-6 py-3 border border-white rounded-lg">
+    <div className="w-full flex gap-3 bg-white/20 text-white pl-5 py-2 border border-white rounded-lg">
 
       <div className="flex flex-col justify-between">
         <div>
-          {launch.success ? (<MdOutlineCheckCircleOutline className="text-green-500 text-4xl" />) : (<MdOutlineCancel className="text-red-500 text-4xl" />)}
+          {launch.success ? (<MdOutlineCheckCircleOutline className="text-green-500 text-3xl" />) : (<MdOutlineCancel className="text-red-500 text-3xl" />)}
         </div>
         <div>
-          <MdFavoriteBorder className="text-white text-4xl" />
+          <MdFavoriteBorder className="text-white text-3xl" />
         </div>
       </div>
 
       <div>
-        <p className="text-xl font-[var(--font-roboto)] font-bold mb-1">{launch.name}</p>
-        <p className="text-xl font-[var(--font-roboto)] font-normal mb-1">{launchSite}</p>
-        <p className="text-xl font-[var(--font-roboto)] font-normal mb-1">{rocketName}</p>
-        <p className="text-xl font-[var(--font-roboto)] font-normal mb-1">{new Date(launch.date_local).toLocaleDateString()}</p>
+        <p className="text-base font-[var(--font-roboto)] font-semibold mb-1 w-35 truncate">{launch.name}</p>
+        <p className="text-base font-[var(--font-roboto)] font-normal mb-1 w-35 truncate">{launchSite}</p>
+        <p className="text-base font-[var(--font-roboto)] font-normal mb-1">{rocketName}</p>
+        <p className="text-base font-[var(--font-roboto)] font-normal mb-1">{new Date(launch.date_local).toLocaleDateString()}</p>
       </div>
 
     </div>
