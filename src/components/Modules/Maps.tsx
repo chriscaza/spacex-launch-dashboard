41493494643a4747349511981705532e1156338f
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2025-05-19 02:01:06 -0600

    bg y rounded de componente maps, solo para observar el espacio de maps

--- src/components/Modules/Maps.tsx
+++ src/components/Modules/Maps.tsx
@@ -1,6 +1,6 @@
 function Maps() {
     return (
-        <div className="bg-red-500 w-full h-full">
+        <div className="bg-blue-300 w-full h-full rounded-2xl">
             Aqui va el mapa
         </div>
     )
